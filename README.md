7c76e9e0ac0c4813c3b40715deecc20445a7b204
Chris Lowe <clowe@luc.edu>
2014-11-25 14:18:42 -0600

    adds testing page, resources. Fixes a bug preventing the js to execute correctly

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 malkovichmalkovich
 ==================
 Shameless iteration of:
-  -[Hodor](https://chrome.google.com/webstore/detail/hodor/ollegclclhehigjllkeabkmkfcjlhdef?hl=en) 
-  -[buzzword-replacerator](https://github.com/afolson/buzzword-replacerator) 
+
+  -[Hodor](https://chrome.google.com/webstore/detail/hodor/ollegclclhehigjllkeabkmkfcjlhdef?hl=en)
+
+  -[buzzword-replacerator](https://github.com/afolson/buzzword-replacerator)
+   
   -[heygirl](https://github.com/keccers/heygirl)
--- content_script.js
+++ content_script.js
@@ -1,10 +1,6 @@
-// Copyright (c) 2014 Chris Lowe.
-
 walk(document.body);
-
 //Walk function stolen from:
 //http://is.gd/mwZp7E
-
 function walk(node){
 	var child, next;
 	switch (node.nodeType){
@@ -22,14 +18,22 @@ function walk(node){
 			malkovichText (node);
 			break;
 	}
+	malkovichImg();
 }
 
 //TODO implement onClick functionality of browser action icon
 //TODO replace images with mystery actor
 
 function malkovichText (textNode) {
-	textNode.nodeValue = textNode.nodeValue.
-		replace(/([A-Z])\w+/g, "Malkovich").//intial capital
-		replace(/\b([A-Z]+)\b/g, "MALKOVICH"). //all caps
-		replace(/\b([a-z]+)\b/g, "malkovich"); //all lower case
+		textNode.nodeValue = textNode.nodeValue.
+			replace(/([A-Z])\w+/g, "Malkovich").	//intial capital
+			replace(/\b([A-Z]+)\b/g, "MALKOVICH").	//all caps
+			replace(/\b([a-z]+)\b/g, "malkovich");	//all lower case
+}
+
+function malkovichImg (){ //TODO randomize image
+	var images = document.getElementsByTagName("img");
+	for (var i = 0, l = images.length; i < l; i++){
+		images[i].src = "http://upload.wikimedia.org/wikipedia/commons/4/45/John_Malkovich_at_San_Sebastian_Filmfestival.jpg";
+	}
 }
